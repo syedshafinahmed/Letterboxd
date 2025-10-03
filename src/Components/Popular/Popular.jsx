e0945a15bd621decfcc040db325c4d579965c804
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-04 03:20:33 +0600

    added pages to popular movies data

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -7,10 +7,17 @@ const Popular = () => {
     console.log(popular);
     return (
         <div className='max-w-6xl mx-auto'>
-            <div className='grid grid-cols-10 gap-y-2 pt-5'>
+            <div className='grid grid-cols-10 gap-y-2 pt-10'>
                 {
                     popular.results?.map(popula => <PopularList key={popula.id} popula={popula}></PopularList>)
                 }
+            </div>
+            <div className='flex gap-x-8 justify-center py-10'>
+                <b className='pageno w-6 h-6 text-center'>1</b>
+                <b className='pageno w-6 h-6 text-center'>2</b>
+                <b className='pageno w-6 h-6 text-center'>3</b>
+                <b className='pageno w-6 h-6 text-center'>4</b>
+                <b className='pageno w-6 h-6 text-center'>5</b>
             </div>
         </div>
     );
