ec28112cdd284aa1efc541b0c0ffa86ae558ce59
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-03 21:19:17 +0600

    added header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Letterboxd from '../../assets/letterboxd.png'
+import Me from '../../assets/orangme.jpg'
 const Header = () => {
     return (
-        <div className='montserrat text-white'>
-            <div className='flex items-center gap-x-2'>
-                <img src={Letterboxd} alt="letterboxd-logo" />
-                <p className='text-4xl font-bold'>Letterboxd</p>
+        <div className='bg-gray-800'>
+            <div className='montserrat text-white max-w-7xl mx-auto flex justify-between'>
+                <div className='flex items-center gap-x-2'>
+                    <img src={Letterboxd} alt="letterboxd-logo" />
+                    <p className='text-4xl font-bold'>Letterboxd</p>
+                </div>
+                <div className='flex items-center gap-x-5'>
+                    <div className='flex items-center gap-x-3'>
+                        <img className='w-6 rounded-full' src={Me} alt="" />
+                        <p>Shafin Ahmed</p>
+                    </div>
+                    <i class="fa-solid fa-bolt"></i>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <button className='btn bg-green-600 border-none font-bold px-4 py-1'><i class="fa-solid fa-plus"></i> LOG</button>
+                </div>
             </div>
         </div>
     );
